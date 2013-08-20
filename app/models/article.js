0002530b04b3fc19df6358da14cf79944a2a77ba
Martin Genev <mgenev@Martins-MacBook-Pro.local>
2013-08-19 22:02:06 -0700

    updated  article schema for 2 sided flashcard

--- app/models/article.js
+++ app/models/article.js
@@ -13,7 +13,8 @@ var mongoose = require('mongoose')
 var ArticleSchema = new Schema({
 	created: {type : Date, default : Date.now},
 	title: {type: String, default: '', trim : true},
-	content: {type: String, default: '', trim : true},
+	side1: {type: String, default: '', trim : true},
+  side2: {type: String, default: '', trim : true},
 	user: {type : Schema.ObjectId, ref : 'User'}
 });
 
