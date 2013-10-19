ff16c1f13483ef310d605b709df9c0d26c8ca313
mgenev <mgenev@gmail.com>
2013-10-19 14:34:28 -0700

    added compass and prefixer

--- gruntfile.js
+++ gruntfile.js
@@ -1,47 +1,79 @@
 module.exports = function(grunt) {
-
-    // Project configuration.
+    // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
+            compass: {
+                files: ['public/sass/{,*/}*.{scss,sass}'],
+                tasks: ['compass:server', 'autoprefixer']
+            },
+            jade: {
+                files: ['app/views/**'],
+                options: {
+                    livereload: true,
+                },
+            },
+            js: {
+                files: ['public/js/**', 'app/**/*.js'],
+                tasks: [],
+                options: {
+                    livereload: true,
+                },
+            },
             html: {
                 files: ['public/views/**'],
                 options: {
                     livereload: true,
                 },
             },
-            js: {
-                files: ['public/js/**'],
-                options: {
-                    livereload: true,
-                },
-
-            },
             css: {
-                files: ['public/sass/**'],
-                tasks: ['compass'],
+                files: ['public/css/**'],
                 options: {
-                    livereload: true,
-                },
+                    livereload: true
+                }
+            }
+        },
+        compass: {
+            options: {
+                sassDir: 'public/sass',
+                cssDir: 'public/css',
+                generatedImagesDir: '.tmp/img/generated',
+                imagesDir: 'public/img',
+                javascriptsDir: 'public/js',
+                // fontsDir: '<%= yeoman.app %>/styles/fonts',
+                importPath: 'public/lib',
+                httpImagesPath: '/img',
+                httpGeneratedImagesPath: '/img/generated',
+                httpFontsPath: '/styles/fonts',
+                relativeAssets: false,
+                assetCacheBuster: false
+            },
+            dist: {
+                options: {
+                    generatedImagesDir: 'dist/img/generated'
+                }
+            },
+            server: {
+                options: {
+                    debugInfo: true
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 1 version']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/styles/',
+                    src: '{,*/}*.css',
+                    dest: '.tmp/styles/'
+                }]
             }
         },
         jshint: {
-            all: ['gruntfile.js']
-        },
-        compass: { // Task
-            dist: { // Target
-                options: { // Target options
-                    sassDir: 'public/sass',
-                    cssDir: 'public/css',
-                    environment: 'production'
-                }
-            },
-            dev: { // Another target
-                options: {
-                    sassDir: 'public/sass',
-                    cssDir: 'public/css'
-                }
-            }
+            // all: ['gruntfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js']
         },
         nodemon: {
             dev: {
@@ -58,30 +90,52 @@ module.exports = function(grunt) {
                     },
                     cwd: __dirname
                 }
-            },
-            exec: {
-                options: {
-                    exec: 'less'
-                }
             }
         },
         concurrent: {
-            target: {
-                tasks: ['nodemon', 'watch'],
+            tasks: ['nodemon', 'watch'], 
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        mochaTest: {
+            options: {
+                reporter: 'spec'
+            },
+            src: ['test/**/*.js']
+        },
+        bower: {
+            install: {
                 options: {
-                    logConcurrentOutput: true
+                    targetDir: './public/lib',
+                    layout: 'byComponent',
+                    install: true,
+                    verbose: true,
+                    cleanBowerDir: true
                 }
             }
         }
     });
 
-    // Load NPM tasks 
-    grunt.loadNpmTasks('grunt-contrib-compass');
+    //Load NPM tasks 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-bower-task');
 
-    // Default task(s).
-    grunt.registerTask('default', ['jshint', 'compass', 'concurrent:target']);
+    //Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
+
+    //Default task(s).
+    // grunt.registerTask('default', ['jshint', 'concurrent']);
+    grunt.registerTask('default', ['concurrent']);
+    //Test task.
+    grunt.registerTask('test', ['mochaTest']);
+
+    //Bower task.
+    grunt.registerTask('install', ['bower']);
 };
